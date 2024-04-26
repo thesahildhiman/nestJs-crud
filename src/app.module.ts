9300ebcb4587492709eef79e43661c2faf2a648a
thesahildhiman <95946799+thesahildhiman@users.noreply.github.com>
2024-04-26 17:38:00 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { UserModule } from './user/user.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://admin:admin@cluster0.3zvmnhy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+      '',
       { dbName: 'crud' },
     ),
     UserModule,
